14fa8ff86d8b617b651b4e59a3ec6a6fc88a1f4e
fullyslick <default@mail.com>
2018-04-24 16:15:20 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Website Distribution
+
+This project contain automation packages for Node.js, that are used before a website is uploaded on a server. The files in `dist` folder of the project, are modified by the **Installed Tools**. These files represents the optimized website ready for upload to the server.
+
+
+## Installed Tools
+
+- **Node.js**
+- **Gulp** - `npm install --global gulp-cli` - command in terminal to install gulp package globally;
+- **gulp-sass** - lready installed in this project. For any project, navigate to project folder using the terminal and type `npm install gulp-sass` to install gulp package locally;
+- **gulp-autoprefixer** - already installed in this project. For any project, navigate to project folder using the terminal and type `npm install gulp-autoprefixer` to install gulp package locally;
+
+
+## Tools Modification
+To use the tools checkout the configuration of the `gulpfile.js`.
---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+// What plugin to use in this case gulp-sassand gulp-autoprefixer
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+// Run the tasks -> name of task as string, function to be executed, (done) as argument for async execution
+gulp.task('default', function(done) {
+  // Add the -> gulp.watch if you want to execute the task on file save or change
+  // So watch for changes and the execute the gulp task
+  gulp.watch('sass/**/*.scss', gulp.series('styles'));
+
+  // Always include done as argument and at the end of task to run the gulp ASYNC
+  done();
+});
+
+// The sequance of task that will be executed.
+// gulp.task -> name of task sequance, and the function containing the tasks that has to be executed.
+// Run 'gulp styles' in terminal to execute the 'styles' serquance.
+gulp.task('styles', function(done) {
+  // First look at some folder for files that will be modified.
+  // In this case look for all folders and subfolders of 'sass', and look for files with extension 'scss'.
+  gulp.src('sass/**/*.scss')
+
+    // Then Run 'sass' plugin, which converts scss to css.
+    .pipe(sass())
+
+    // After that Run autoprefixer, using the last 4 versions of all browsers, settings.
+    .pipe(autoprefixer({
+      browsers: ['last 4 versions'],
+      cascade: false
+    }))
+
+    // Save the converted files in 'dist/css' folder
+    .pipe(gulp.dest('dist/css'))
+
+  console.log('Completed');
+  // Always include done as argument and at the end of task to run the gulp ASYNC
+  done();
+});
